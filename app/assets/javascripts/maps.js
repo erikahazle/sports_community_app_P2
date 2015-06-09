de8930c112232679052ba54b2d8179d61d078f0d
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-06-09 15:58:11 +0100

    Added a nav with home button

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -17,7 +17,6 @@ $(document).ready(function() {
   
   if($('#map-canvas').length > 0) {
     google.maps.event.addDomListener(window, 'load', initialize);
-
     sport_id = $('h2').attr('data-id');
     console.log("sport_id is: " + sport_id);
     $.ajax({
