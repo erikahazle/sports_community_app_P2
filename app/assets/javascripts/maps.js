d2243f8eb5547c1561b1449905a854b5cdee045b
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-06-09 14:20:17 +0100

    Sport page displays pins for all of the community locations

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,42 +1,23 @@
 console.log('Connected')
 
 // GOOGLE MAPS
+var infowindow = null;
+var map
 
-var markerCount = 0;
-var map;
- 
-//Initializes the map…
 function initialize() {
-    var myLatlng = new google.maps.LatLng(46.855141, -96.8372664);
-    var map_canvas = document.getElementById('map_canvas');
-    var map_options = {
-        center: myLatlng,
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(map_canvas, map_options);
-}   
- 
- 
-//This function will add a marker to the map each time it
-//is called.  It takes latitude, longitude, and html markup
-//for the content you want to appear in the info window
-//for the marker.
-function addMarkerToMap(lat, long){
-    debugger;
-    var infowindow = new google.maps.InfoWindow();
-    var myLatLng = new google.maps.LatLng(lat, long);
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-    });  
+  var myLatlng = new google.maps.LatLng(51.5085300,-0.1257400);
+  var mapOptions = {
+    zoom: 10,
+    center: myLatlng
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 $(document).ready(function() {
   
   if($('#map-canvas').length > 0) {
-    initialize();
+    google.maps.event.addDomListener(window, 'load', initialize);
+
     sport_id = $('h2').attr('data-id');
     console.log("sport_id is: " + sport_id);
     $.ajax({
@@ -45,7 +26,11 @@ $(document).ready(function() {
       dataType: 'json'
     }).done(function(data) {
       $.each(data, function(index, data) {
-        // addMarkerToMap(data.latitude, data.longitude);
+        var latLng = new google.maps.LatLng(data.latitude, data.longitude);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
       })
     })
   }
